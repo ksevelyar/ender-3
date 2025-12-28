2ff0fc525fbb7165de3ecab217e13cb8f10e4706
Sergey Zubkov <ksevelyar@protonmail.com>
2025-12-28 20:09:00 +0300

    reduce linear advance

--- marlin-2.1.2.5/Marlin/Configuration.h
+++ marlin-2.1.2.5/Marlin/Configuration.h
@@ -1514,7 +1514,7 @@
  *     O-- FRONT --+
  */
 /*#define NOZZLE_TO_PROBE_OFFSET { -45, -8, 0 }*/
-#define NOZZLE_TO_PROBE_OFFSET { -42, -5, -2.85 }
+#define NOZZLE_TO_PROBE_OFFSET { -42, -5, -2.95 }
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
--- marlin-2.1.2.5/Marlin/Configuration_adv.h
+++ marlin-2.1.2.5/Marlin/Configuration_adv.h
@@ -2115,10 +2115,11 @@
  */
 #define LIN_ADVANCE
 #if ENABLED(LIN_ADVANCE)
+  // NOTE: shorter bowden
   #if ENABLED(DISTINCT_E_FACTORS)
-    #define ADVANCE_K { 0.56 }    // (mm) Compression length per 1mm/s extruder speed, per extruder
+    #define ADVANCE_K { 0.3 }    // (mm) Compression length per 1mm/s extruder speed, per extruder
   #else
-    #define ADVANCE_K 0.56        // (mm) Compression length applying to all extruders
+    #define ADVANCE_K 0.3        // (mm) Compression length applying to all extruders
   #endif
   //#define ADVANCE_K_EXTRA       // Add a second linear advance constant, configurable with M900 L.
   //#define LA_DEBUG              // Print debug information to serial during operation. Disable for production use.
